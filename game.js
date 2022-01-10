ec68d0d611264dbef6a8716525d8f03e6c61194b
nipun rajpal <nipunrajpal999@gmail.com>
2022-01-10 22:16:29 +0530

    uploading website game

---
+++ game.js
@@ -0,0 +1,68 @@
+let music = new Audio("music.mp3");
+let audioturn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X";
+let isgameover = false;
+
+
+// function to change the turn
+let changeturn = ()=>{
+    return turn === "X" ? "0": "X";
+}
+
+
+// function to check for win
+let checkwin = ()=>{
+    let boxtext  = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6], 
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e =>{
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + "  WON";
+            isgameover = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+            document.querySelector(".line").style.width = "20nw";
+        }
+    })
+}
+
+
+// game  logic
+// music.play();
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if (boxtext.innerText === '') 
+        {
+            boxtext.innerText = turn;
+            turn = changeturn();
+            audioturn.play();
+            checkwin();
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
+        }
+    })
+})
+
+
+reset.addEventListener('click', ()=>{
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"
+    isgameover = false
+        document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+})
